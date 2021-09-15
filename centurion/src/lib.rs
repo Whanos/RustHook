497c4cfd6e9f47e916e87ebf5619778260203fac
Whanos <github@sergal.fun>
2021-09-15 10:38:26 +0100

    er

--- centurion/src/lib.rs
+++ centurion/src/lib.rs
@@ -1,9 +1,28 @@
 #![no_std]
-
 use core::panic::PanicInfo;
+use winapi::shared::ntdef::{HANDLE, BOOLEAN};
+use core::ffi::c_void;
+use winapi::ctypes::c_ulong;
+
+
+type PcreateProcessNotifyRoutine = extern "system" fn(
+    parent_id: HANDLE,
+    process_id: HANDLE,
+    create: BOOLEAN
+) -> c_void;
+
+type PsSetCreateProcessNotifyRoutine = extern "system" fn(
+    notify_routine: PcreateProcessNotifyRoutine,
+    remove: BOOLEAN
+) -> c_ulong;
 
 fn main() {
-    
+    let routine_callback: PcreateProcessNotifyRoutine;
+    let routine = PsSetCreateProcessNotifyRoutine(routine_callback, )
+}
+
+fn pcreate_process_notify_routine(parent_id: HANDLE, process_id: HANDLE, create: BOOLEAN) {
+
 }
 
 // Panic handler
@@ -15,5 +34,6 @@ fn panic(_info: &PanicInfo) -> ! {
 // Driver entry point
 #[no_mangle]
 pub extern "system" fn driver_entry() -> u32 {
+    main();
     0 /* STATUS_SUCCESS */
 }
